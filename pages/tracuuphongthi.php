123c321501ec5a087409869d1ff191a6f623a92f
Huy0521 <you@example.com>
2022-12-18 19:29:05 +0700

    trang tra cuu diem thi, tra cuu phong thi

---
+++ pages/tracuuphongthi.php
@@ -0,0 +1,331 @@
+<?php
+include "../connectdb.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script type="text/javascript" src="../js/swfobject.js"></script>
+    <script type="text/javascript" src="../Slide/jquery-1.6.4.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/default.css" />
+    <link rel="stylesheet" type="text/css" href="../menu/ddlevelsfiles/ddlevelsmenu-base.css" />
+    <link rel="stylesheet" type="text/css" href="../menu/ddlevelsfiles/ddlevelsmenu-topbar.css" />
+    <script type="text/javascript" src="../menu/ddlevelsfiles/ddlevelsmenu.js"></script>
+    <meta name="robots" content="INDEX,FOLLOW,ALL" /><meta name="language" content="Vietnamese, VIETNAM, vietnam" /><meta name="author" content="www.thietkephanmem.com" /><meta name="copyright" content="Copyright (C) 2006-2010 thietkephanmem.com" /><meta name="revisit-after" content="1 day" /><meta name="document-rating" content="General" /><meta name="document-distribution" content="Global" /><meta name="distribution" content="Global" /><meta name="area" content="Network Solution" /><meta name="placename" content="vietnam" /><meta name="resource-type" content="Document" /><link rel="shortcut icon" href="../favicon.ico" /><meta name="Googlebot" content="index" /><meta http-equiv="imagetoolbar" content="no" /><meta http-equiv="Page-Exit" content="progid:DXImageTransform.Microsoft.Fade(duration=.2)" /><meta name="classification" content="www.thietkephanmem.com, Vietnam" />
+
+
+    <div id="fb-root"></div>
+    <script>    (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1";
+            fjs.parentNode.insertBefore(js, fjs);
+        } (document, 'script', 'facebook-jssdk'));</script>
+    <script type="text/javascript">
+        //<![CDATA[
+        var theForm = document.forms['aspnetForm'];
+        if (!theForm) {
+            theForm = document.aspnetForm;
+        }
+        function __doPostBack(eventTarget, eventArgument) {
+            if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+                theForm.__EVENTTARGET.value = eventTarget;
+                theForm.__EVENTARGUMENT.value = eventArgument;
+                theForm.submit();
+            }
+        }
+        //]]>
+    </script>
+
+
+    <script src="/WebResource.axd?d=B9d5BfrBx6qbQK4g5vCWMa0wgZPD6BgZpvZxUGDtJgTOLK9AQ9bjWkWNC6fl5RkwNUCCQHrjXeGRYE1Z9V6745MSqtw1&amp;t=637814912020000000" type="text/javascript"></script>
+
+
+    <script src="/ScriptResource.axd?d=07LxvDx_6dQ5Mh4NZZzI5a9xhjDCAzjbjjCuNCkOsT2LC9DuSJSexk-665mM4MPKs3ojjksYoR5YTySovkMjlxNdlslXjYYbnJ3SBbsaxgRZF2ik_zH8oeaUnexkD1PbdyzRG1pj6oyh2MxND7mMODwScxhKi4J64zkjkHbhtVBAyQnQ0&amp;t=49337fe8" type="text/javascript"></script>
+    <script type="text/javascript">
+        //<![CDATA[
+        if (typeof(Sys) === 'undefined') throw new Error('ASP.NET Ajax client-side framework failed to load.');
+        //]]>
+    </script>
+
+    <script src="/ScriptResource.axd?d=GI_luv1d2-roP1ygtjsSizb4kRHWsKxaTh3vVN-lauoPGfDETB8d3m9K7e-tvuZN-9U2gOAVGNIpFNnTuKlyHP9KKuflL8kTvXfCe8y-AKCfASkLRY8MVEZu1lKDUc-qn58aq_nyVV-6bEW8-uPQHpXDBU_ACWi990HrpY7MDPRGJPBCKCpT2NmnMpSXHsH77336qA2&amp;t=49337fe8" type="text/javascript"></script>
+    <div>
+
+        <input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="3CF293FC" />
+        <input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEdAAxvmGyd0ngEf2Yx6kf9dpSOjz4b6uJwYpZbH8ThP2f/J08/4v+jK1cJjC20h4DMK8Ij6m0bJ/Cvi9ZYXOBvF3Ovn4+6H73ptvlgtIKXOHNgZmDOAmCJe55MXIQzoCxrtRrmHSw9sAPxzwuezCn92PQ9FX2+G88atdWCMvJoE2VuImfvvCKlZQFb+4ZQN1W6CJUb2VushK1NJn5Of9fa3CJ/HvwLYhWqvHMDtKyOwUbGgA189Vgk1czyHbVW/YbkhpPZR8MGt5/jwzZSdVs8V+tT7iPtHA==" />
+    </div>
+    <div align="center">
+        <table border="0" cellpadding="0" cellspacing="0" width="996px">
+            <tr>
+                <td>
+                    <table border="0" cellpadding="0" cellspacing="0" width="996px">
+                        <tr>
+                            <td class="head-bg">
+            <span id="ctl00_MenuVi1_LabelHead">
+<a href="/"><img src="../Images/3bannerhnue.jpg" border="0" width="996" height="150" ></a></span>
+                            </td>
+                        </tr>
+                        <tr>
+
+                            <td class="menu-bg-head">
+
+            <span id="ctl00_MenuVi1_LabelMenu"><div id="ddtopmenubar" class="mattblackmenu">
+<ul>
+<li><a href="/" title="Trang chủ">Trang chủ</a></li>
+<li><a rel="ddsubmenu1" href="/gioi-thieu_g0.aspx" title="Giới thiệu">Giới thiệu</a></li>
+<li><a href="/lich-tuan.aspx" title="Lịch tuần">Lịch tuần</a></li>
+
+<li><a rel="ddsubmenu5559" href="/tin-tuc_t9.aspx" title="TIN TỨC">TIN TỨC</a></li>
+<ul id="ddsubmenu5559" class="ddsubmenustyle">
+<li><a href="/hoat-dong-noi-bo_t15.aspx" title="Hoạt động nội bộ">Hoạt động nội bộ</a></li>
+<li><a href="/hoi-nghi-hoi-thao_t14.aspx" title="Hội nghị - Hội thảo">Hội nghị - Hội thảo</a></li>
+<li><a href="/hoat-dong-doan_t16.aspx" title="Hoạt động Đoàn">Hoạt động Đoàn</a></li>
+<li><a href="/hoc-bong-du-hoc_t69.aspx" title="Học bổng - Du học">Học bổng - Du học</a></li>
+</ul>
+
+<li><a rel="ddsubmenu55512" href="/55-nam-csp_t12.aspx" title="55 Năm CSP">55 Năm CSP</a></li>
+<ul id="ddsubmenu55512" class="ddsubmenustyle">
+<li><a href="/thu-ngo_t25.aspx" title="Thư ngỏ">Thư ngỏ</a></li>
+<li><a href="/ky-niem-55-nam-csp_t67.aspx" title="Kỷ niệm 55 Năm CSP">Kỷ niệm 55 Năm CSP</a></li>
+</ul>
+
+
+<li><a rel="ddsubmenu3" href="/thong-bao.aspx" title="Đào tạo - Tuyển sinh">Đào tạo - Tuyển sinh</a></li>
+<li><a rel="ddsubmenu2" href="/thu-vien.aspx" title="Thư viện">Thư viện</a></li>
+<li class="End"><a href="/lien-he.aspx" title="Liên hệ">Liên hệ</a></li>
+<ul></div>
+<div id="ddsubmenu1" class="ddsubmenustyle">
+<li><a href="/gioi-thieu-chung_h40.aspx" title="Giới thiệu chung">Giới thiệu chung</a></li>
+<li><a href="/bang-vang-thanh-tich_h41.aspx" title="Bảng vàng thành tích">Bảng vàng thành tích</a></li>
+<li><a href="/50-nam-csp_h65.aspx" title="50 Năm CSP">50 Năm CSP</a></li>
+</div>
+<div id="ddsubmenu2" class="ddsubmenustyle">
+<li><a href="/tai-lieu.aspx" title="Tài liệu tham khảo">Tài liệu tham khảo</a></li>
+<li><a href="/video-clip.aspx" title="Video Clip">Video Clip</a></li>
+<li><a href="/photo-album.aspx" title="Photo Album">Photo Album</a></li>
+</div>
+<div id="ddsubmenu3" class="ddsubmenustyle">
+<li><a href="/thoi-khoa-bieu.aspx" title="Thời khóa biểu học sinh">Thời khóa biểu học sinh</a></li>
+<li><a href="/thong-bao.aspx" title="Thông tin tuyển sinh">Thông tin tuyển sinh</a></li>
+<li><a href="/phong-thi.aspx" title="Tra cứu phòng thi">Tra cứu phòng thi</a></li>
+<li><a href="/diem-thi.aspx" title="Tra cứu điểm thi">Tra cứu điểm thi</a></li>
+</div>
+</span>
+                                <script type="text/javascript">
+                                    ddlevelsmenu.setup("ddtopmenubar", "topbar") //ddlevelsmenu.setup("mainmenuid", "topbar|sidebar")
+                                </script>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="bg-head2">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr>
+                                        <td class="head-ngay">
+                                            <span id="ctl00_MenuVi1_LabelHotline">Thứ Tư, ngày 30 tháng 11 năm 2022</span>
+                                        </td>
+                                        <td style="text-align: right; width: 204px;">
+                                            <input name="ctl00$MenuVi1$TextBoxSearch" type="text" value="Từ khóa..." id="ctl00_MenuVi1_TextBoxSearch" class="text-search" onfocus="if (this.value==&#39;Từ khóa...&#39;) this.value=&#39;&#39;;" onblur="if (this.value==&#39;&#39;) this.value=&#39;Từ khóa...&#39;;" />
+                                        </td>
+                                        <td style="width: 47px;">
+                                            <input type="image" name="ctl00$MenuVi1$ImageButtonSearch" id="ctl00_MenuVi1_ImageButtonSearch" src="../Images/index_13.jpg" style="height:31px;width:47px;border-width:0px;" />
+                                        </td>
+
+                                    </tr>
+
+                                </table>
+                            </td>
+                        </tr>
+
+                    </table>
+
+                </td>
+            </tr>
+            <tr>
+                <td style="background:#ffffff; padding-top:4px; padding-bottom:4px;" valign="top">
+
+                    <table border="0" width="100%" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td class="body-main" valign="top">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr>
+                                        <td class="bg-t1">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <form method="post" action="">
+                                            <td class="bg-t3">
+                                            <a>Họ và tên </a><input type="text" name="name">
+                                            <a>Số điện thoại </a><input type="number" name="phone">
+                                            <a>Ngày sinh </a><input type="date" name="birth">
+                                            <input type="submit" value="Tra cứu" name="gui">
+                                        </td>
+                                        </form>
+                                      
+                                    </tr>
+                                </table>
+                                <style>
+                                        .bang
+                                        {
+                                          border:1px solid black;
+                                        }
+                                        </style>
+                                <table style="width:100%">
+                                    <tr>
+                                        <th class="bang">STT</th>
+                                        <th class="bang">SBD</th>
+                                        <th class="bang">Họ và tên</th>
+                                        <th class="bang">Số điện thoại</th>
+                                        <th class="bang">Ngày sinh</th>
+                                        <th class="bang">Giới tính</th>
+                                        <th class="bang">Phòng thi văn</th>
+                                        <th class="bang">Phòng thi toán</th>
+                                        <th class="bang">Phòng thi chuyên</th> 
+                                    </tr>
+
+                                <?php
+                                function getTenMon($id_phong)
+                                {
+                                    GLOBAL $conn;
+                                    $tenMon = '';
+                                    $sql = "SELECT * FROM phong_thi WHERE id_phong_thi = '$id_phong'";
+                                    $result = mysqli_query($conn, $sql);
+                                    if(mysqli_num_rows($result) > 0){
+                                        $tenMon = mysqli_fetch_array($result)['ten_phong'];
+                                    }
+                                    return $tenMon;
+                                }
+                                if (isset($_POST['gui'])) 
+                                {
+                                    if($_POST['name'] != "" && $_POST['phone'] != "")
+                                    {
+                                    $sql = "SELECT * FROM `hoc_sinh` WHERE `ten` = '$_POST[name]' AND `phone`= '$_POST[phone]'";
+                                        $do = mysqli_query($conn, $sql);
+                                        $i=0;
+                                        if (mysqli_num_rows($do) > 0) 
+                                        {
+                                            while ($row = mysqli_fetch_assoc($do)) 
+                                            {
+                                                $i++;
+                                                ?>
+                                 <tr>
+                                     <td class="bang"><?php echo $i; ?></td>
+                                      <td class="bang"><?php echo $row['id_hoc_sinh']; ?></td>
+                                       <td class="bang"><?php echo $row['ten']; ?></td>
+                                        <td class="bang"><?php echo $row['phone'] ; ?></td> 
+                                        <td class="bang"><?php echo $row['ngay_sinh']; ?></td>
+                                          <td class="bang"><?php echo $row['gioi_tinh']; ?></td>
+                                         <td class="bang"><?php echo getTenMon($row['id_phong_thi_van']) ; ?></td> 
+                                         <td class="bang"><?php echo getTenMon($row['id_phong_thi_toan']) ; ?></td>
+                                         <td class="bang"><?php echo getTenMon($row['id_phong_thi_chuyen']) ; ?></td> 
+                                 </tr>
+                                                    <?php 
+                                            }
+                                        }
+                                    }
+                                       else if ($_POST['name'] != "" || $_POST['phone'] != "") 
+                                    {
+                                        $i=0;
+                                         $sql = "SELECT * FROM `hoc_sinh` WHERE `ten` = '$_POST[name]' or`phone`= '$_POST[phone]'";
+                                        $do = mysqli_query($conn, $sql);
+                                        if (mysqli_num_rows($do) > 0) 
+                                        {
+                                            while ($row = mysqli_fetch_assoc($do)) 
+                                            {
+                                                $i++;
+                                                ?>
+                                 <tr>
+                                     <td class="bang"><?php echo $i; ?></td>
+                                      <td class="bang"><?php echo $row['id_hoc_sinh']; ?></td>
+                                       <td class="bang"><?php echo $row['ten']; ?></td>
+                                        <td class="bang"><?php echo $row['phone'] ; ?></td> 
+                                        <td class="bang"><?php echo $row['ngay_sinh']; ?></td>
+                                          <td class="bang"><?php echo $row['gioi_tinh']; ?></td>
+                                         <td class="bang"><?php echo getTenMon($row['id_phong_thi_van']) ; ?></td> 
+                                         <td class="bang"><?php echo getTenMon($row['id_phong_thi_toan']) ; ?></td>
+                                         <td class="bang"><?php echo getTenMon($row['id_phong_thi_chuyen']) ; ?></td>                                       
+                                 </tr>
+                                                    <?php 
+                                            }
+                                        }
+                                    }
+                                     $conn->close();
+                                }
+                             ?>
+                                </table>
+                       
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+            </tr>
+            <tr>
+                <td class="menu-f">
+                <span id="ctl00_LabelMenuFoot"><div id="ddtopmenubar" class="mattblackmenu2">
+<ul>
+<li class="End"><a href="/" title="Trang chủ">Trang chủ</a></li>
+<li><a href="/gioi-thieu_g0.aspx" title="Giới thiệu">Giới thiệu</a></li>
+<li><a href="/lich-tuan.aspx" title="Lịch tuần">Lịch tuần</a></li>
+<li><a href="/tai-lieu.aspx" title="Tài liệu tham khảo">Tài liệu tham khảo</a></li>
+<li><a href="/thoi-khoa-bieu.aspx" title="Thời khóa biểu">Thời khóa biểu</a></li>
+<li><a href="/phong-thi.aspx" title="Tra cứu phòng thi">Tra cứu phòng thi</a></li>
+<li><a href="/diem-thi.aspx" title="Tra cứu điểm thi">Tra cứu điểm thi</a></li>
+<li><a href="/lien-he.aspx" title="Liên hệ">Liên hệ</a></li>
+<ul></div>
+</span>
+                </td>
+            </tr>
+            <tr>
+                <td class="footer">
+                    <table border="0" cellpadding="0" cellspacing="0" width="996px">
+                        <tr>
+                            <td style="padding:35px 10px 0 138px; width:50%; vertical-align:top;">
+                                <a href="/dang-nhap.aspx">Đăng nhập</a>
+                                |
+                                <a href="/sitemap.aspx">Site map</a>
+                                |
+                                <a href="mailto:suphamchuyen@gmail.com">Góp ý</a>
+                                |
+                                <a href="#">Lên đầu</a>
+                                <br />
+
+                                <table border="0" cellpadding="0" cellspacing="2" width="100%">
+                                    <tr>
+                                        <td style="padding-top:15px;">
+                                            <span id="ctl00_h_LabelLuotHomNay">Hôm nay: 5 lượt | Tổng truy cập: 14499277 lượt</span>        </td>
+                                    </tr>
+
+
+                                </table>
+
+
+                            </td>
+                            <td style="padding:25px 10px 0 50px; width:50%; vertical-align:top;">
+                            <span id="ctl00_LabelFoot"><strong>Trường THPT Chuy&ecirc;n Đại học Sư phạm<br />
+</strong><br />
+Địa chỉ: Nh&agrave; D - 136 Xu&acirc;n Thuỷ - Cầu Giấy - H&agrave; Nội.<br />
+TEL:&nbsp;<span style="font-size: 17.3333px;">(024</span><span style="font-size: 17.3333px;">3</span><span style="font-size: 17.3333px;">) 75.47.661</span>&nbsp;FAX: <span id="ctl00_ContentPlaceHolder1_LabelDetail"><span lang="EN-US" style="font-size:13.0pt">(024<span style="font-size: 17.3333px;">3</span>) 75.47.661</span></span><br />
+Email: c3chuyendhsp@hanoiedu.vn<br />
+<br /></span>
+                            </td>
+                        </tr>
+                    </table>
+
+
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+
+    <script type="text/javascript">
+        //<![CDATA[
+        Sys.Application.add_init(function() {
+            $create(Sys.UI._UpdateProgress, {"associatedUpdatePanelId":"ctl00_ContentPlaceHolder1_UpdatePanel1","displayAfter":500,"dynamicLayout":true}, null, null, $get("ctl00_ContentPlaceHolder1_UpdateProgress1"));
+        });
+        //]]>
+    </script>
+</form>
+</body>
+</html>
